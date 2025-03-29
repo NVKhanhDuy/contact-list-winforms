066bf6624bdc0173c775259e7808d92543364237
Khanh Duy <nguyenduy548433@gmail.com>
2025-03-29 13:38:14 +0700

    Sua Form them lien he, bo sung truong Email

--- ContactList/AddContactForm.Designer.cs
+++ ContactList/AddContactForm.Designer.cs
@@ -34,71 +34,88 @@
             textBox4 = new TextBox();
             button1 = new Button();
             button2 = new Button();
+            textBox5 = new TextBox();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(334, 67);
+            textBox1.Location = new Point(210, 40);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
+            textBox1.Size = new Size(106, 23);
             textBox1.TabIndex = 0;
             textBox1.Text = "Ngô Thành Được";
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(300, 197);
+            textBox2.Location = new Point(210, 118);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(205, 31);
+            textBox2.Size = new Size(145, 23);
             textBox2.TabIndex = 0;
             textBox2.Text = "Nguyễn Văn Khánh Duy";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(595, 67);
+            textBox3.Location = new Point(416, 40);
+            textBox3.Margin = new Padding(2, 2, 2, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(150, 31);
+            textBox3.Size = new Size(106, 23);
             textBox3.TabIndex = 0;
             textBox3.Text = "2280600750";
             textBox3.TextChanged += textBox1_TextChanged;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(595, 197);
+            textBox4.Location = new Point(416, 118);
+            textBox4.Margin = new Padding(2, 2, 2, 2);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(205, 31);
+            textBox4.Size = new Size(145, 23);
             textBox4.TabIndex = 0;
             textBox4.Text = "2280600480";
             // 
             // button1
             // 
-            button1.Location = new Point(909, 131);
+            button1.Location = new Point(210, 202);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(78, 20);
             button1.TabIndex = 1;
             button1.Text = "Lưu";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(595, 309);
+            button2.Location = new Point(351, 202);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(112, 34);
+            button2.Size = new Size(112, 20);
             button2.TabIndex = 1;
             button2.Text = "Thêm Liên Hệ";
             button2.UseVisualStyleBackColor = true;
             // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(524, 199);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 2;
+            textBox5.Text = "Email";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1058, 450);
+            ClientSize = new Size(741, 270);
+            Controls.Add(textBox5);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox4);
             Controls.Add(textBox2);
             Controls.Add(textBox3);
             Controls.Add(textBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -113,5 +130,6 @@
         private TextBox textBox4;
         private Button button1;
         private Button button2;
+        private TextBox textBox5;
     }
 }
